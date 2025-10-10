c8211577b6efdaca1c0263a07b8fffb6dfe1fa86
MarianD <mdadzie5@gmail.com>
2025-10-10 16:24:14 -0500

    modified terminal file

--- terminal_command.sh
+++ terminal_command.sh
@@ -18,5 +18,8 @@ curl -X POST http://localhost:8000/folders -H "Content-Type: application/json" -
 # delete a folder
  curl -X DELETE http://localhost:8000/folders/1 -H "Content-Type: application/json" -d '{ "title": "fourth_folder" }' | jq
 
-# add items in a folder
+# add items into a folder
 curl -X POST http://localhost:8000/folders/1/items -H "Content-Type: application/json" -d '{ "title": "first_item" }' | jq
+
+# get all items in a folder
+ curl http://localhost:8000/folders/1/items | jq
