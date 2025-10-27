f460b097a8e4de349ac43e308fe6ff1970e41ab1
MarianD <mdadzie5@gmail.com>
2025-10-27 15:36:42 -0500

    added javascript code to frontend folder

---
+++ frontend/script.js
@@ -0,0 +1,33 @@
+const apiURL= 'http://localhost:8000';
+
+async function makeAPICall(http_method, api_path, payload=null){
+    const url = `${apiURL}${api_path}`
+    const method = http_method.toUpperCase()
+    if (method == 'GET'){
+        try { 
+            const result = await fetch(url)
+            return result
+        } catch(ex){
+            console.log(ex)
+            return null
+        }
+    }
+    try { 
+        const result = await fetch(url, {
+            method: method, 
+            body: payload,
+            headers: {
+                'Content-Type': 'text/javascript'
+            }
+        })
+        return result
+    } catch(ex){
+        console.log(ex)
+        return null
+    }
+}
+
+(async () => {
+	const apiResult = await makeAPICall("GET","/folders");
+	console.log(apiResult);
+ })();
