c9d5a35a07b4fe86232f9f74fdb8eb93dd1f5e61
MarianD <mdadzie5@gmail.com>
2026-01-07 14:03:49 -0600

    wip fastapi test suite

--- backend/tests/fastapi_tool/test_fastapi.py
+++ backend/tests/fastapi_tool/test_fastapi.py
@@ -2,15 +2,31 @@ import pytest
 from fastapi.testclient import TestClient
 from fastapi_tool.fastapi import app 
 from sqlalchemy.orm import Session
+from library.models import get_db, Folder
+import uuid
 
 
 @pytest.fixture
-def test_client():
+def test_client(testing_db_session: Session):
     with TestClient(app) as client:
+        app.dependency_overrides[get_db] = lambda: testing_db_session
         yield client
 
-def test_get_folders_success(testing_db_session: Session, test_client: TestClient):
-    response = test_client.get("/folders")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
+class TestGetFoldersEndPoint:
+    
+    def test_get_default_empty_folders_success(test_client: TestClient):
+        response = test_client.get("/folders")
+        assert response.status_code == 200
+        assert response.json() == []
 
+    def test_get_active_folders_success(test_client: TestClient, testing_db_session: Session):
+        #arrange
+        random_title = f"test_{uuid.uuid4()}"
+        folder = Folder(title=random_title)
+
+        #act
+
+        #assert
+
+
+#def test_create_folder_success(test_client: TestClient):
