b0ecd6a51df9bd53aeb89e01f553ca9556ca749b
MarianD <mdadzie5@gmail.com>
2026-01-08 14:49:58 -0600

    added more api test

--- backend/tests/fastapi_tool/test_fastapi.py
+++ backend/tests/fastapi_tool/test_fastapi.py
@@ -12,21 +12,37 @@ def test_client(testing_db_session: Session):
         app.dependency_overrides[get_db] = lambda: testing_db_session
         yield client
 
-class TestGetFoldersEndPoint:
+def create_test_folder(testing_db_session: Session, random_title:str=f"test_{uuid.uuid4()}", is_folder_deleted:bool=False) -> Folder:
+    folder = Folder(title=random_title, is_deleted=is_folder_deleted)
+    testing_db_session.add(folder)
+    testing_db_session.commit()
+    return folder
+
     
-    def test_get_default_empty_folders_success(test_client: TestClient):
-        response = test_client.get("/folders")
-        assert response.status_code == 200
-        assert response.json() == []
+def test_get_default_empty_folders_success(test_client: TestClient):
+    response = test_client.get("/folders")
+    assert response.status_code == 200
+    assert response.json() == []
 
-    def test_get_active_folders_success(test_client: TestClient, testing_db_session: Session):
-        #arrange
-        random_title = f"test_{uuid.uuid4()}"
-        folder = Folder(title=random_title)
+def test_get_active_folders_success(test_client: TestClient, testing_db_session: Session):
+    #arrange
+    folder = create_test_folder(testing_db_session)
+    #act
+    response = test_client.get("/folders")
+    #assert
+    assert response.status_code == 200
+    res_json = response.json()
+    assert len(res_json) == 1
+    assert res_json[0]["title"] == folder.title
 
-        #act
-
-        #assert
+def test_get_deleted_folders_not_in_response(test_client: TestClient, testing_db_session: Session):
+    #arrange
+    create_test_folder(testing_db_session, is_folder_deleted=True)
+    #act
+    response = test_client.get("/folders")
+    #assert
+    assert response.status_code == 200
+    assert response.json() == []
 
 
 #def test_create_folder_success(test_client: TestClient):
